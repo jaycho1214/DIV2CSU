e853747b3f503c362706340e32aa150202592c5a
sfhk1234 <118529644+sfhk1234@users.noreply.github.com>
2023-10-05 23:38:47 +0900

    상점 벌점 변수 각각 생성 및 웹페이지 칸 나눔

--- src/app/points/components/TotalPointBox.tsx
+++ src/app/points/components/TotalPointBox.tsx
@@ -13,23 +13,16 @@ export function TotalPointBox({ user }: { user: Soldier }) {
     fetchTotalPoints(user.sn).then((d) => setData(d));
   }, [user.sn]);
 
+  var meritPoint = 0;
+  var deMeritPoint = 0;
+
+  if (data.verifiedPoint < 0) {
+    deMeritPoint += data.verifiedPoint;
+  } else {
+    meritPoint += data.verifiedPoint;
+  }
   return (
     <Row>
-      <Card className='flex-1'>
-        <Skeleton
-          paragraph={{ rows: 1 }}
-          active
-          loading={data?.unverifiedPoint == null || data?.verifiedPoint == null}
-        >
-          <Statistic
-            title={`총 점수 (${user.name})`}
-            value={`${
-              parseInt(data?.verifiedPoint ?? '0', 10) +
-              parseInt(data?.unverifiedPoint ?? '0', 10)
-            }점`}
-          />
-        </Skeleton>
-      </Card>
       <Card className='flex-1'>
         <Skeleton
           paragraph={{ rows: 1 }}
@@ -52,7 +45,21 @@ export function TotalPointBox({ user }: { user: Soldier }) {
         >
           <Statistic
             title='승인 상점'
-            value={`${data?.verifiedPoint}점`}
+            value={`${meritPoint}점`}
+            prefix={<CheckOutlined />}
+            valueStyle={{ color: '#cf1322' }}
+          />
+        </Skeleton>
+      </Card>
+      <Card className='flex-1'>
+        <Skeleton
+          paragraph={{ rows: 1 }}
+          active
+          loading={data?.verifiedPoint == null}
+        >
+          <Statistic
+            title='승인 벌점'
+            value={`${deMeritPoint}점`}
             prefix={<CheckOutlined />}
             valueStyle={{ color: '#cf1322' }}
           />

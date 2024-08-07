0452108b6f215c9517c0820c2b5a3f34a9e55605
Jay Cho <jaycho1214@gmail.com>
2024-08-06 18:29:49 -0700

    주석 추가

--- src/interfaces/soldiers.ts
+++ src/interfaces/soldiers.ts
@@ -1,25 +1,44 @@
 import z from 'zod';
 import { Permission } from './permissions';
 
+/**
+ * 시스템에서 병사를 나타냅니다.
+ */
 export const Soldier = z.object({
+  // 군번이자 ID (Military ID)
   sn: z
     .string()
     .trim()
-    .regex(/^\d{2}-\d{5,8}$/, { message: '잘못된 형식의 군번입니다' }),
+    .regex(/^\d{2}-\d{5,8}$/, { message: '유효하지 않은 군번 형식입니다' }),
+
+  // 비밀번호 (Password)
   password: z
     .string()
     .trim()
-    .min(1, { message: '비밀번호는 최소 1자리 입니다.' })
-    .max(30, { message: '비밀번호는 최대 30자리 입니다.' }),
+    .min(1, { message: '비밀번호는 최소 1자 이상이어야 합니다' })
+    .max(30, { message: '비밀번호는 최대 30자까지 가능합니다' }),
+
+  // 병사 (enlisted) / 장교 (nco)
   type: z.enum(['enlisted', 'nco']),
+
+  // 이름 (Name)
   name: z
     .string()
     .trim()
-    .min(1, { message: '이름은 최소 1자리 입니다.' })
-    .max(5, { message: '이름은 최대 5자리 입니다.' }),
+    .min(1, { message: '이름은 최소 1자 이상이어야 합니다' })
+    .max(5, { message: '이름은 최대 5자까지 가능합니다' }),
+
+  // 유저 인증 날짜 / 인증되지 않은 경우 null
   verified_at: z.date().nullable(),
+
+  // 유저 인증 거부 날짜 / 인증되지 않은 경우 null
   rejected_at: z.date().nullable(),
+
+  // 유저 삭제 날짜 / 삭제되지 않은 경우 null
+  // 상점 기록을 추적하기 위해 DB에서 삭제하지 않고 deleted_at을 사용
   deleted_at: z.date().nullable(),
+
+  // 유저 권한
   permissions: z.array(Permission),
 });
 
